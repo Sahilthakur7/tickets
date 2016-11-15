794c05a106ffea3cbd49f19b1545b38e8fdbaed2
sahilrd07 <sahilrd07@gmail.com>
2016-11-15 17:52:56 +0530

    create new project completed

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,10 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-
+root "projects#index"
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
+resources :projects
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
