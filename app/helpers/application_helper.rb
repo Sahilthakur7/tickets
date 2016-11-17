e1f647d8d577a65b44278e2d5e97045f3b7eca4c
sahilrd07 <sahilrd07@gmail.com>
2016-11-17 14:37:57 +0530

    add title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def title(*parts)
+        unless parts.empty?
+            content_for :title do
+                (parts << "Ticketee").join(" - ")
+            end
+        end
+    end
 end
+
