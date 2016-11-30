beafe7135af636a41466b4c9edaf57f1e50b552f
sahilrd07 <sahilrd07@gmail.com>
2016-11-30 23:26:04 +0530

    Tickets done

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -35,6 +35,11 @@ class TicketsController < ApplicationController
         end
    end
 
+   def destroy
+       @ticket.destroy
+       flash[:notice]= "Ticket has been deleted"
+       redirect_to @project
+   end
     private
 
 
