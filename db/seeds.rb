16dc4d22ab5c999f5a43c019a9d5a0ab0072fd45
sahilrd07 <sahilrd07@gmail.com>
2017-01-17 15:44:18 +0530

    Added admin flag to User model, and seeded the first admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,13 @@
 unless User.exists?(email: "admin@ticketee.com")
     User.create!(email: "admin@ticketee.com", password: "password", admin: true)
 end
+
+unless User.exists?(email: "viewer@ticketee.com")
+    User.create!(email: "viewer@ticketee.com", password: "password")
+end
+
+["Subllime Text 3", "Internet Explorer"].each do |name|
+    unless Project.exists?(name: name)
+        Project.create!(name: name, description: "A sample project about #{name}")
+    end
+end
