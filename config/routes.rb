b782b4de572b0e4000dacb4a886aba7083770d76
sahilrd07 <sahilrd07@gmail.com>
2017-01-21 17:41:23 +0530

    Add ability to add users

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+
   namespace :admin do
   root 'application#index'
 
   resources :projects, only: [:new, :create, :destroy]
+
+  resources :users
   end
 
   devise_for :users
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -48,6 +48,7 @@ RSpec.configure do |config|
   # https://relishapp.com/rspec/rspec-rails/docs
 config.include Warden::Test::Helpers,type: :feature
 config.after(type: :feature) { Warden.test_reset! }
-
+config.include Devise::TestHelpers, type: :controller
+config.include Devise::TestHelpers, type: :view
   config.infer_spec_type_from_file_location!
 end
