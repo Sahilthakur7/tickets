1a7cf2b2cf2cf9840e1e5b650d83c069fcf69442
sahilrd07 <sahilrd07@gmail.com>
2017-01-24 16:10:24 +0530

    added feature of archibing users

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,12 @@ Rails.application.routes.draw do
 
   resources :projects, only: [:new, :create, :destroy]
 
-  resources :users
+  resources :users do
+    member do
+        patch :archive
+    end
+  end
+
   end
 
   devise_for :users
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161224101938) do
+ActiveRecord::Schema.define(version: 20170123165308) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20161224101938) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "admin",                  default: false
+    t.datetime "archived_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
