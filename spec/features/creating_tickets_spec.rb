a47e161998653a0144596bb85d3551a144cc69af
sahilrd07 <sahilrd07@gmail.com>
2017-02-07 15:21:11 +0530

    Caching the file upload

--- spec/features/creating_tickets_spec.rb
+++ spec/features/creating_tickets_spec.rb
@@ -40,4 +40,18 @@ RSpec.feature "Users can create new tickets" do
             expect(page).to have_content "speed.txt"
         end
     end
+
+    scenario "persisting gile uploads across form displays" do
+        attach_file "File", "spec/fixtures/speed.txt"
+        click_button "Create Ticket"
+
+        fill_in "Name", with: "Add documentation for blink tag"
+        fill_in "Description", with: "The blink tag has a speed attribute"
+        click_button "Create Ticket"
+        within(
+            '#ticket .attachment'
+        ) do
+            expect(page).to have_content "speed.txt"
+        end
+    end
 end
